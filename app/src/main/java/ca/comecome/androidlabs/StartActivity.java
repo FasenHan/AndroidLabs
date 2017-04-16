6cf0877e39b088d5caf4036f6b3f24803165f77e
fasen <fasen_xu_han@outlook.com>
2017-04-15 22:49:56 -0400

    Added three activities in lab2

--- app/src/main/java/ca/comecome/androidlabs/StartActivity.java
+++ app/src/main/java/ca/comecome/androidlabs/StartActivity.java
@@ -1,13 +1,34 @@
 package ca.comecome.androidlabs;
 
+import android.app.Activity;
+import android.content.Context;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
+
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_start);
+
+
     }
 }
